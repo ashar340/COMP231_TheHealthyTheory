1037739edece03934ec7a6b2288bb1e894a38292
Ananya Sharma <ananyas237@gmail.com>
2020-02-19 06:26:55 -0500

    Angular-ionic skelton

--- server/app.js
+++ server/app.js
@@ -1,16 +1,22 @@
 var createError = require('http-errors');
-var express = require('express');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var express = require('express');
+var app = express();
+
+app.use((req, res, next) => {
+    res.status(200).json({
+        message: 'Server works!'
+    });
+});
+
+module.exports= app;
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var connectDB = require('./config/db');
-var app = express();
-app.get('/', (req, res) => res.send('Hello from the server!'));
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
 
 module.exports = app;
